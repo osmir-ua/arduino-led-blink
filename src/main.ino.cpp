80c3d112859ffd03fba0e45cfef00cc5729f985d
Oleksii Myronchenko <osmir-ua@users.noreply.github.com>
2019-12-05 17:35:37 +0200

    unneeded variables is removed

--- src/main.ino.cpp
+++ src/main.ino.cpp
@@ -1,26 +1,17 @@
+# 1 "C:\\Users\\osmir\\AppData\\Local\\Temp\\tmpc6x687xt"
+#include <Arduino.h>
+# 1 "C:/Users/osmir/Documents/Arduino/LED-blinking/arduino-led-blink/src/main.ino"
 #include <Arduino.h>
 
 #define LED 13
 
-// #define light_time_period 100
-// #define idle_time_period 1000
-// #define ON_state "1"
-// #define OFF_state "0"
 
-#define time_quantum 100 //quantum of time (in milliseconds). Used in Binking pattern matrix
 
-/* 
-* Binking pattern matrix
-* 
-* Matrix is set of blinking pattern arrays
-*
-* Each of them is array of booleans 
-*  - 1 (or other any nonzero value) turns LED ON for quantum of time
-*  - 0 turns LED OFF for quantum of time   
-*
-* Matrix index is set of blinking pattern arrays labels
-*/
 
+
+
+#define time_quantum 100
+# 24 "C:/Users/osmir/Documents/Arduino/LED-blinking/arduino-led-blink/src/main.ino"
 String blinkMatrixIndex[] = {
   "Вимкнути",
   "Просте мигання",
@@ -35,23 +26,25 @@ bool blinkMatrix[][500] = {
   {1,0,1,0,1,0, 1,1,1,0,1,1,1,0,1,1,1,0, 1,0,1,0,1,0}
 };
 
-// ---------------------------------
+
 unsigned int currentState;
 
 unsigned int arrayIndex;
 unsigned int arrayPosIndex;
 
 unsigned long lastTime = millis();
-
-
+void setup();
+void processBlink(unsigned int patternId);
+void loop();
+#line 47 "C:/Users/osmir/Documents/Arduino/LED-blinking/arduino-led-blink/src/main.ino"
 void setup() {
 
     pinMode(LED,OUTPUT);
-    
+
     Serial.begin(9600);
     Serial.print("Привіт...\n");
     Serial.print("Починаємо працювати...\n-------------------\n");
-    
+
     Serial.println(".... МЕНЮ ....");
 
     Serial.print("sizeof(blinkMatrixIndex) = "); Serial.println(sizeof(blinkMatrixIndex));
@@ -64,57 +57,57 @@ void setup() {
     Serial.println();
     Serial.print("Сurrent state is "); Serial.write(currentState); Serial.println();
 
-    
+
 }
 
 
 void processBlink(unsigned int patternId){
 
   unsigned long currentTime = millis();
-  
-  // if patternId is null of time interval doesn't pass -- do nothing
+
+
   if(!patternId || (currentTime - lastTime) < time_quantum ) return;
 
   if (!arrayIndex || patternId != arrayIndex) {
     arrayIndex = patternId;
-    
+
     if (!blinkMatrixIndex[arrayIndex]) return;
-    
+
     arrayPosIndex = 0;
   }
 
   if (blinkMatrixIndex[arrayIndex][arrayPosIndex]){
-    
+
     if (blinkMatrixIndex[arrayIndex][arrayPosIndex] == 0) digitalWrite(LED_BUILTIN, LOW);
     else digitalWrite(LED_BUILTIN, HIGH);
 
-    lastTime = currentTime;    
+    lastTime = currentTime;
     arrayPosIndex++;
 
     if (!blinkMatrixIndex[arrayIndex][arrayPosIndex]) arrayPosIndex = 0;
   }
 }
 
-// nvl()
+
 
 void loop() {
     String data;
     unsigned int patternID;
-    
+
     if (Serial.available() > 0){
       data = Serial.readString();
-      
+
       if (isdigit(data.charAt(0))) {
         patternID = (unsigned int)data.toInt();
       }
 
-      // status message
-      Serial.print("Entered : "); 
-        Serial.print(data);  
-        Serial.print(" ("); 
+
+      Serial.print("Entered : ");
+        Serial.print(data);
+        Serial.print(" (");
         Serial.write(patternID);
         Serial.println(")");
-      
+
 
       if (patternID == currentState) {
         Serial.print("Нічого не змінилось. ");
