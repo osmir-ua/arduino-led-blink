72033a6bea417385a35145596f9130d39c9543db
Oleksii Myronchenko <osmir-ua@users.noreply.github.com>
2019-12-05 17:37:14 +0200

    unneeded vars is really removed

--- src/main.ino
+++ src/main.ino
@@ -2,11 +2,6 @@
 
 #define LED 13
 
-// #define light_time_period 100
-// #define idle_time_period 1000
-// #define ON_state "1"
-// #define OFF_state "0"
-
 #define time_quantum 100 //quantum of time (in milliseconds). Used in Binking pattern matrix
 
 /* 
